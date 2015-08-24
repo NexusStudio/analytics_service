6c50d04a62b0579e4a191c70426ea8365eaf2e25
mpjr1 <mapg.jr@gmail.com>
2015-08-24 21:48:32 +0000

    Updated Gemfile for deployment to Heroku

--- Gemfile
+++ Gemfile
@@ -43,4 +43,13 @@ group :development, :test do
   gem 'spring'
 end
 
+group :production do
+   gem 'pg'
+   gem 'rails_12factor'
+ end
+ 
+ group :development do
+   gem 'sqlite3'
+ end
+
 gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'welcome/index'
 
   get 'welcome/about'
   
+  root "application#hello"
   root to: 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
